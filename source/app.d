5e14cefa7f0b3d9cc4d2ba9c9859514a2f3ba757
RealDoigt <57451013+RealDoigt@users.noreply.github.com>
2024-04-27 16:59:32 -0400

    Update app.d

--- source/app.d
+++ source/app.d
@@ -4,18 +4,38 @@ import raylib;
 
 void main()
 {
-    auto ad = AudioDevice.getInstance();
-    while (!ad.isReady){}
-    ad.setVolume(1);
+    // auto ad = AudioDevice.getInstance();
+    // while (!ad.isReady){}
+    // ad.setVolume(1);
+    // 
+    // auto kirby1 = new MusicStream("music/01 Main Title.mp3");
+    // kirby1.setVolume(1);
+    // kirby1.play;
+    // 
+    // writeln(kirby1.isPlaying);
+    // 
+    // while(kirby1.getTimePlayed < kirby1.getTimeLength){}
+    // kirby1.stop;
+    // 
+    // ad.close;
+    
+    InitAudioDevice(); // Initialize audio device
 
-    auto kirby1 = new MusicStream("music/01 Main Title.mp3");
-    kirby1.setVolume(1);
-    kirby1.play;
+    Music kirby1 = LoadMusicStream("music/01 Main Title.mp3"); // Load music
+    SetMusicVolume(kirby1, 1); // Set volume
+    PlayMusicStream(kirby1); // Play music
 
-    writeln(kirby1.isPlaying);
+    // Print whether music is playing
+    while (!IsMusicStreamPlaying(kirby1)) {
+        // Wait for music to start playing
+    }
 
-    while(kirby1.getTimePlayed < kirby1.getTimeLength){}
-    kirby1.stop;
+    // Wait until music finishes playing
+    while (GetMusicTimePlayed(kirby1) < GetMusicTimeLength(kirby1)) {
+        // Keep waiting
+    }
 
-    ad.close;
+    StopMusicStream(kirby1); // Stop music
+
+    CloseAudioDevice(); // Close audio device
 }
